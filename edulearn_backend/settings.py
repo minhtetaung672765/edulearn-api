b757fb85d88bd5ec89c9850f787d0d2aaade5806
Min Htet Aung <minhtetaung672765@gmail.com>
2025-04-29 16:52:26 +0630

    .env shifted

--- edulearn_backend/settings.py
+++ edulearn_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ AUTH_USER_MODEL = 'core.User'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mm40i1_1dgu=dt2whin9@22-1xf4q&0yh!_%5d@$qcn6qjmr0i'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
